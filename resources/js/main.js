d83a2f5f60eb64e40620cfbcfd507f6d6878a0e5
DesignRuse <sonjeeb.kumar.das@outlook.com>
2023-11-29 22:59:56 +0530

    Commit
    Commit

---
+++ resources/js/main.js
@@ -0,0 +1,19 @@
+﻿window.addEventListener('DOMContentLoaded', function () {
+
+	var stylings = document.getElementsByClassName('styling');
+
+	for (var i = 0; i < stylings.length; i++) {
+		stylings[i].addEventListener('click', async function (e) {
+			e.preventDefault();
+
+			var link = this.parentElement.href;
+			console.log(link);
+			try {
+				await navigator.clipboard.writeText(link);
+				alert("Copied the text: " + link);
+			} catch (err) {
+				console.error(err);
+			}
+		});
+	}
+});
